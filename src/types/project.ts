8aadf8b10587a49914946f406c8c5543c5516e26
Spandan-Mishra <spandanmishra69@gmail.com>
2026-03-14 00:07:55 +0530

    feat: SingleProjectChip and project types added

---
+++ src/types/project.ts
@@ -0,0 +1,32 @@
+import type { WikimediaUsername } from "./_";
+
+export interface Project {
+    logoUrl: string;
+    name: string;
+    projectId: string;
+    projectLeads: WikimediaUsername[];
+    url: string;
+
+}
+export type ProjectCreate = Project;
+
+export type ProjectUpdate = Project & { projectLeads: WikimediaUsername[] };
+export const initialProjectCreate: ProjectCreate = {
+    logoUrl: "",
+    name: "",
+    projectId: "",
+    projectLeads: [],
+    url: ""
+}
+export const projectCreateReducer = (state: ProjectCreate, action: Partial<ProjectCreate>) => {
+    return {
+        ...state,
+        ...action
+    }
+}
+export const projectUpdateReducer = (state: ProjectUpdate, action: Partial<ProjectUpdate>) => {
+    return {
+        ...state,
+        ...action
+    }
+}
